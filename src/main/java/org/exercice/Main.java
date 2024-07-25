d1ce5f51b0a5dca60d3072c72101400ed36525f2
Jean-Charles Audebert <jean-charles.audebert@maif.fr>
2024-07-25 16:47:32 +0200

    Continue cases

--- src/main/java/org/exercice/Main.java
+++ src/main/java/org/exercice/Main.java
@@ -3,6 +3,7 @@ package org.exercice;
 import org.exercice.models.building.Village;
 import org.exercice.models.lieu.LieuxPossibles;
 import org.exercice.models.unite.Villageois;
+import org.exercice.utils.DisplayUtil;
 import org.exercice.utils.VillageInitializer;
 import org.exercice.utils.VillageUtils;
 
@@ -14,6 +15,7 @@ public class Main {
         villageois.setLieu(LieuxPossibles.MAISON);
         villageois.showUnite(villageois.getName());
 
+        //DisplayUtil displayVillage(tatouin);
         System.out.println(villageois.getDescription());
         System.out.println("-------------Village----------------");
         System.out.println(VillageUtils.getSummary(tatouin));
